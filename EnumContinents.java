a224ac4dd807eec379fbc0316f819da4a4caa3c2
ajeesha123456 <ajeeshaazees@gmail.com>
2025-02-04 13:03:10 +0100

    Add files via upload

---
+++ EnumContinents.java
@@ -0,0 +1,22 @@
+package day18exception_enums;
+
+public class EnumContinents {
+    public static void main(String[] args) {
+
+        for(Continents continents:Continents.values()){
+            System.out.println(continents);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
